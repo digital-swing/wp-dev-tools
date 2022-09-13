560b6b68c42a52a3389dfe22470064c8f0c53d82
Lucas Demea <lucas@digital-swing.com>
2022-09-13 20:39:35 +0200

    chore: context

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo $1
-echo $2
-echo $3 
 curl --header "Authorization: token $1" \
   --header 'Accept: application/vnd.github.v3.raw' \
   --output /tmp/main.yml \
@@ -12,9 +9,6 @@ PHP_VERSION=$(cat /tmp/main.yml | grep php_version | cut -d'"' -f 2)
 
 current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo $1
-echo $2
-echo $3 
 sed -i "s/##repo##/$2/" $current_path/continue-config.yml
 sed -i "s/##branch##/$3/" $current_path/continue-config.yml
 sed -i "s/##php-version##/$PHP_VERSION/" $current_path/continue-config.yml
